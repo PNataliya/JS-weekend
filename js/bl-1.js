a7634f0c9d205472759177915b27dacb65ebd693
Nataliya Pogrebniak <pogrebnyak.nataliya@gmail.com>
2022-06-25 11:50:15 +0300

    meet 2

--- js/bl-1.js
+++ js/bl-1.js
@@ -4,11 +4,11 @@
 // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
 // 70 === 01:10
 
-let minute = prompt();
-let hour = Math.floor(minute / 60);
-let stringHour = String(hour).padStart(2, 0);
-let stringMinute = String(minute % 60).padStart(2, 0);
-const time = `${stringHour} часов : ${stringMinute} минут`;
-console.log(time);
+// let minute = prompt();
+// let hour = Math.floor(minute / 60);
+// let stringHour = String(hour).padStart(2, 0);
+// let stringMinute = String(minute % 60).padStart(2, 0);
+// const time = `${stringHour} часов : ${stringMinute} минут`;
+// console.log(time);
 
 //console.log(typeof hour);
---
+++ js/bl-2.js
@@ -0,0 +1,35 @@
+// const a = {};
+// const b = {};
+// const c = {
+//   d: 1,
+//   e: 4,
+// };
+
+// c[a] = 3;
+// c[b] = 4;
+// console.log(c);
+
+// const result1 = 'hello' ?? 'world';
+// console.log(result1);
+
+// const result2 = 0 ?? 5;
+// console.log(result2);
+
+// const result3 = '' ?? 'javascript';
+// console.log(result3);
+
+// const result4 = false ?? true;
+// console.log(result4);
+
+// const result5 = null ?? 'not null';
+// console.log(result5);
+
+// const result6 = undefined ?? 'defined';
+// console.log(result6);
+
+// let message = null;
+// const hello = 'Hi JavaScript';
+// const result7 = message ?? hello;
+// console.log(result7);
+// const result6 = null ?? NaN;
+// console.log(result6);
